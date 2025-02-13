a02e48434baa93a6a23eb1624d343e61b51eb181
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2025-02-13 18:58:34 +0530

    ng modules

--- AngularJS/app/app.js
+++ AngularJS/app/app.js
@@ -1,13 +1,36 @@
-var myNinjaApp=angular.module("myNinjaApp",[]);
+var myNinjaApp = angular.module("myNinjaApp", []);
 let a;
-myNinjaApp.config(function(){
-  a=10;
+myNinjaApp.config(function () {
+  a = 10;
 });
 
-myNinjaApp.run(function(){
+myNinjaApp.run(function () {
   console.log(a);
 });
 
-myNinjaApp.controller(function(){
-
-})
+myNinjaApp.controller("NinjaController", [
+  "$scope",
+  function ($scope) {
+    $scope.myFav = "red";
+    $scope.ninjas = [
+      {
+        fName: "Hattori",
+        belt: "green",
+        rate: 900,
+        available: true
+      },
+      {
+        fName: "Kasama",
+        belt: "orange",
+        rate: 1000,
+        available: false
+      },
+      {
+        fName: "ryi",
+        belt: "blc",
+        rate: 800,
+        available: true
+      },
+    ];
+  },
+]);
